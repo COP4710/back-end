43c7d6aa710d9258bcbebe3dee481068568374b8
Dimitri Rivera <dimitririvera2@gmail.com>
2020-11-19 13:25:58 -0500

    Updated sql to fit new requirements
    - Got rid of location pictures
    - Got rid of ower field in event location
    - Added more attributes to events
    - Got rid of invitations since all events are now assumed public events
    - Moved existing columns around for ease of understanding

--- ExhibitDB.sql
+++ ExhibitDB.sql
@@ -10,6 +10,11 @@ USE `ExhibitDB`;
 CREATE TABLE IF NOT EXISTS `ExhibitDB`.`User` (
     `username` VARCHAR(200) NOT NULL,
     `password` VARCHAR(200) NOT NULL,
+    -- Could have a boolean instead
+    -- `super_admin TINYINT NOT NULL DEFAULT 'false',
+
+    -- The char was made for the purposes of having three roles, but if we just combine the admin and person role, which it seems
+    -- we can, then we can just have two roles. Super admins and not super admins.
     `role` CHAR(1) NOT NULL DEFAULT 'p',
     PRIMARY KEY (`username`)
 );
@@ -19,20 +24,15 @@ CREATE TABLE IF NOT EXISTS `ExhibitDB`.`User` (
 -- Table `ExhibitDB`.`EventLocation`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ExhibitDB`.`EventLocation` (
-    `location` VARCHAR(200) NOT NULL,
-    `location_id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(200) NOT NULL,
+    `location` VARCHAR(200) NOT NULL,
     `description` TEXT(1000) NULL,
-    `area_size` VARCHAR(100) NULL,
     `people_capacity` INT NULL,
-    -- `owner` VARCHAR(200) NOT NULL,
+    `area_size` VARCHAR(100) NULL,
+    `location_id` INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (`location`),
-    INDEX(`location_id`),
-    -- CONSTRAINT `OwnerID_EventLocation`
-    --     FOREIGN KEY (`owner`)
-    --     REFERENCES `ExhibitDB`.`User` (`username`)
-    --     ON DELETE CASCADE
-    --     ON UPDATE NO ACTION
+    INDEX(`location_id`)
+    -- No check necessary here since we are determining this data. 
 );
 
 
@@ -40,21 +40,23 @@ CREATE TABLE IF NOT EXISTS `ExhibitDB`.`EventLocation` (
 -- Table `ExhibitDB`.`Event`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ExhibitDB`.`Event` (
-    `event_id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(200) NOT NULL,
     `category` VARCHAR(200) NULL,
     `description` TEXT(1000) NULL,
-    `time` TIME NOT NULL,
-    `date` DATE NOT NULL,
     `phone_number` VARCHAR(20) NULL,
     `email_address` VARCHAR(200) NULL,
+    `num_attendees` INT NOT NULL,
+    `start_time` TIME NOT NULL,
+    `end_time` TIME NOT NULL,
+    `date` DATE NOT NULL,
+    `approved` TINYINT NOT NULL DEFAULT 0,
     `host_id` VARCHAR(200) NOT NULL,
     `location_id` INT NOT NULL,
-    `public` TINYINT NOT NULL DEFAULT 0,
-    `approved` TINYINT NOT NULL DEFAULT 0,
+    `event_id` INT NOT NULL AUTO_INCREMENT,
+
     -- Name and time and date and location
-    -- There should never be an event with the same name happening at the same time, date, and location as another.
-    PRIMARY KEY(`name`, `time`, `date`, `location_id`),
+    -- There should never be an event with the same name starting at the same time, date, and location as another.
+    PRIMARY KEY(`name`, `start_time`, `date`, `location_id`),
     INDEX(`event_id`),
     CONSTRAINT `HostID_Event`
         FOREIGN KEY (`host_id`)
@@ -65,27 +67,11 @@ CREATE TABLE IF NOT EXISTS `ExhibitDB`.`Event` (
         FOREIGN KEY (`location_id`)
         REFERENCES `ExhibitDB`.`EventLocation` (`location_id`)
         ON DELETE CASCADE
-        ON UPDATE NO ACTION
-);
-
-
--- -----------------------------------------------------
--- Table `ExhibitDB`.`Invitation`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ExhibitDB`.`Invitation` (
-    `invited_id` VARCHAR(200) NOT NULL,
-    `event_id` INT NOT NULL,
-    PRIMARY KEY(`invited_id`, `event_id`),
-    CONSTRAINT `EventID_Invitation`
-        FOREIGN KEY (`event_id`)
-        REFERENCES `ExhibitDB`.`Event` (`event_id`)
-        ON DELETE CASCADE
         ON UPDATE NO ACTION,
-    CONSTRAINT `InvitedUserID_Invitation`
-        FOREIGN KEY (`invited_id`)
-        REFERENCES `ExhibitDB`.`User` (`username`)
-        ON DELETE CASCADE
-        ON UPDATE NO ACTION
+    -- This ensures that the int is not overflowed
+    -- However, we could make this a front end check so the sql request isn't even triggered. 
+    CONSTRAINT `Attendees_Limit`
+        CHECK (num_attendees > 0 AND num_attendees <= 2147483646)
 );
 
 
@@ -112,11 +98,12 @@ CREATE TABLE IF NOT EXISTS `ExhibitDB`.`EventAttendee` (
 -- -----------------------------------------------------
 -- Table `ExhibitDB`.`EventReview`
 -- -----------------------------------------------------
+-- Make sure to check if the commenter is an attendee
 CREATE TABLE IF NOT EXISTS `ExhibitDB`.`EventReview` (
-    `commenter_id` VARCHAR(200) NOT NULL,
-    `event_id` INT NOT NULL,
     `rating` INT NULL,
     `comment` TEXT(1000) NULL,
+    `commenter_id` VARCHAR(200) NOT NULL,
+    `event_id` INT NOT NULL,
     -- A user can only comment once on a single event
     PRIMARY KEY(`commenter_id`, `event_id`),
     CONSTRAINT `EventID_EventReview`
@@ -133,30 +120,12 @@ CREATE TABLE IF NOT EXISTS `ExhibitDB`.`EventReview` (
 
 
 -- -----------------------------------------------------
--- Table `ExhibitDB`.`LocationPicture`
--- -----------------------------------------------------
--- Do we want multiple pictures or just one picture?
--- CREATE TABLE IF NOT EXISTS `ExhibitDB`.`LocationPicture` (
---     `location_id` INT NOT NULL,
---     `image_val` BIGINT NOT NULL,
---     `picture` LONGBLOB NULL,
---     -- This is kind of gross. Have to compare longblob's
---     PRIMARY KEY(`location_id`, `image_val`),
---     CONSTRAINT `LocationID_LocationPicture`
---         FOREIGN KEY (`location_id`)
---         REFERENCES `ExhibitDB`.`EventLocation` (`location_id`)
---         ON DELETE CASCADE
---         ON UPDATE NO ACTION
--- );
-
-
--- -----------------------------------------------------
 -- Table `ExhibitDB`.`EventProposed`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ExhibitDB`.`EventProposed` (
     `location_id` INT NOT NULL,
     `event_id` INT NOT NULL,
-    -- Event can only be proposed to one 
+    -- Event can only be proposed to one location at a time
     PRIMARY KEY (`location_id`, `event_id`),
     CONSTRAINT `EventID_EventProposed`
         FOREIGN KEY (`event_id`)
